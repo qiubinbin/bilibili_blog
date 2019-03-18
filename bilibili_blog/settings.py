29fbb1ad1be179a67b17727374b339e050b1dd59
qiubinbin <qiubin780061109@163.com>
2019-03-18 23:42:26 +0800

    使用模板嵌套进行定制

--- bilibili_blog/settings.py
+++ bilibili_blog/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '_vn*pi4t@v6v8#7no6v9c&em-m_g$le^9!pf^fl$=+mbnqg&9r'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -55,7 +53,9 @@ ROOT_URLCONF = 'bilibili_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),  # 指向全局模板文件夹bilibili_blog/templates
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bilibili_blog.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
@@ -80,7 +79,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -100,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -113,7 +110,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
--- blog/views.py
+++ blog/views.py
@@ -6,13 +6,13 @@ def blog_list(request):  # Create your views here.
     context = {}
     context['blogs'] = Blog.objects.all()
     context['blog_count'] = Blog.objects.all().count()
-    return render_to_response('blog_list.html', context)
+    return render_to_response('blog/blog_list.html', context)
 
 
 def blog_detail(request, blog_id):
     context = {}
     context['blog'] = get_object_or_404(Blog, id=blog_id)
-    return render_to_response('blog_detail.html', context)
+    return render_to_response('blog/blog_detail.html', context)
 
 
 def blog_with_type(request, blog_type_id):
@@ -20,4 +20,4 @@ def blog_with_type(request, blog_type_id):
     blog_type=get_object_or_404(BlogType,pk=blog_type_id)
     context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type']=blog_type
-    return render_to_response('blog_type_items.html', context)
+    return render_to_response('blog/blog_type_items.html', context)
