1c6cd7fb6b8bcec7ff014effc5294d27e083eccb
qiubinbin <qiubin780061109@163.com>
2019-03-18 23:14:03 +0800

    使用模板嵌套进行定制

--- blog/admin.py
+++ blog/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import Blog, BlogType
 
 
-@admin.register(BlogType)
+@admin.register(BlogType)  # 注册模板
 class BlogTypeAdmin(admin.ModelAdmin):  # Register your models here.
     list_display = ('id', 'type_name')
 
 
-@admin.register(Blog)
+@admin.register(Blog)  # 注册模板
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('title', 'blog_type', 'author', 'create_time', 'last_update')
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, get_object_or_404
+from .models import Blog,BlogType
 
-# Create your views here.
+
+def blog_list(request):  # Create your views here.
+    context = {}
+    context['blogs'] = Blog.objects.all()
+    context['blog_count'] = Blog.objects.all().count()
+    return render_to_response('blog_list.html', context)
+
+
+def blog_detail(request, blog_id):
+    context = {}
+    context['blog'] = get_object_or_404(Blog, id=blog_id)
+    return render_to_response('blog_detail.html', context)
+
+
+def blog_with_type(request, blog_type_id):
+    context = {}
+    blog_type=get_object_or_404(BlogType,pk=blog_type_id)
+    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
+    context['blog_type']=blog_type
+    return render_to_response('blog_type_items.html', context)
